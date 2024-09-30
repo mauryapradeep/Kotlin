17ddde9f1dcc152bbae8446342e7649418f24178
Pradeep Maurya <mauryapradeep814@gmail.com>
2024-09-30 13:18:38 +0000

    update

--- Basics/first.kt
+++ Basics/first.kt
@@ -1,3 +1,24 @@
 fun main(){
-    println("Hello world")
+
+    // to initialise a variable with 'VAR' keyword would behave like  mutable or can be updated
+    // **********   VAR vs VAL
+    var name = "Pradeep Kumar"
+    // print the output
+    //println("Hii, My name is ${name}");
+
+    // update the name variable
+    name = "Mandeep Maurya"
+    //println("Hii, My name is ${name}");
+
+/// while  'val' intialised variable is immutable and can't be updated 
+val rollNo = 33223;
+// rollNo = 89999;   // throw an error as Val are immutable, can't updated
+println("My rool no is ${rollNo}")
+
+
+
+
+
+
+   
 }
